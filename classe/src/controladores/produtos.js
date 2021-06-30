f6825a45c39631f5d7399739cd315246a593c996
iostsicaro <iostsicaro@gmail.com>
2021-06-29 21:18:13 -0300

    Desafio Modulo III

---
+++ classe/src/controladores/produtos.js
@@ -0,0 +1,129 @@
+const knex = require('../conexao');
+
+const listarProdutos = async (req, res) => {
+    const { usuario } = req;
+    const { categoria } = req.query;
+
+    try {
+        let produtos;
+
+        if (categoria) {
+            produtos = await knex('produtos').whereILike({ categoria: categoria })
+        } else {
+            produtos = await knex('produtos').where({ usuario_id: usuario.id }).first();
+        }
+
+        return res.status(200).json(produtos);
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
+
+const obterProduto = async (req, res) => {
+    const { usuario } = req;
+    const { id } = req.params;
+
+    try {
+        const produto = await knex('produtos').where({ id: id, usuario_id: usuario.id}).first();
+
+        if (!produto) {
+            return res.status(404).json('Produto não encontrado');
+        }
+
+        return res.status(200).json(produto);
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
+
+const cadastrarProduto = async (req, res) => {
+    const { usuario } = req;
+    const { nome, estoque, preco, categoria, descricao, imagem } = req.body;
+
+    if (!nome) {
+        return res.status(404).json('O campo nome é obrigatório');
+    }
+
+    if (!estoque) {
+        return res.status(404).json('O campo estoque é obrigatório');
+    }
+
+    if (!preco) {
+        return res.status(404).json('O campo preco é obrigatório');
+    }
+
+    if (!descricao) {
+        return res.status(404).json('O campo descricao é obrigatório');
+    }
+
+    try {
+        const produto = await knex('produtos').insert({ usuario_id: usuario.id, nome: nome, estoque: estoque, preco: preco, categoria: categoria, descricao: descricao, imagem: imagem}).returning('*');
+
+        if (!produto) {
+            return res.status(400).json('O produto não foi cadastrado');
+        }
+
+        return res.status(200).json(produto);
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
+
+const atualizarProduto = async (req, res) => {
+    const { usuario } = req;
+    const { id } = req.params;
+    const { nome, estoque, preco, categoria, descricao, imagem } = req.body;
+
+    if (!nome && !estoque && !preco && !categoria && !descricao && !imagem) {
+        return res.status(404).json('Informe ao menos um campo para atualizaçao do produto');
+    }
+
+    try {
+        const produto = await knex('produtos').where({ id: id, usuario_id: usuario.id});
+
+        if (!produto) {
+            return res.status(404).json('Produto não encontrado');
+        }
+
+        const produtoAtualizado = await knex('produtos').update({ nome: nome, estoque: estoque, preco: preco, categoria: categoria, descricao: descricao, imagem: imagem }).where({ id: id }).returning('*');
+
+        if (!produtoAtualizado) {
+            return res.status(400).json("O produto não foi atualizado");
+        }
+
+        return res.status(200).json('produto foi atualizado com sucesso.');
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
+
+const excluirProduto = async (req, res) => {
+    const { usuario } = req;
+    const { id } = req.params;
+
+    try {
+        const produto = await knex('produtos').where({ id: id, usuario_id: usuario.id}).first();
+
+        if (!produto) {
+            return res.status(404).json('Produto não encontrado');
+        }
+
+        const produtoExcluido = await knex('produtos').del().where({ id: id });
+
+        if (!produtoExcluido) {
+            return res.status(400).json("O produto não foi excluido");
+        }
+
+        return res.status(200).json('Produto excluido com sucesso');
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
+
+module.exports = {
+    listarProdutos,
+    obterProduto,
+    cadastrarProduto,
+    atualizarProduto,
+    excluirProduto
+}
---
+++ classe/src/controladores/usuarios.js
@@ -0,0 +1,78 @@
+const knex = require('../conexao');
+const bcrypt = require('bcrypt');
+
+const cadastrarUsuario = async (req, res) => {
+    const { nome, email, senha, nome_loja } = req.body;
+
+    if (!nome) {
+        return res.status(404).json("O campo nome é obrigatório");
+    }
+
+    if (!email) {
+        return res.status(404).json("O campo email é obrigatório");
+    }
+
+    if (!senha) {
+        return res.status(404).json("O campo senha é obrigatório");
+    }
+
+    if (!nome_loja) {
+        return res.status(404).json("O campo nome_loja é obrigatório");
+    }
+
+    try {
+        const verificarEmail = await knex('usuarios').where({ email: email }).first();
+
+        if (verificarEmail) {
+            return res.status(400).json("O email já existe");
+        }
+
+        const senhaCriptografada = await bcrypt.hash(senha, 10);
+
+        const usuario = await knex('usuarios').insert({ nome: nome, email: email, senha: senhaCriptografada, nome_loja: nome_loja }).return('*');
+
+        if (!usuario) {
+            return res.status(400).json("O usuário não foi cadastrado.");
+        }
+
+        return res.status(200).json(usuario);
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
+
+const obterPerfil = async (req, res) => {
+    return res.status(200).json(req.usuario);
+}
+
+const atualizarPerfil = async (req, res) => {
+    const { nome, email, senha, nome_loja } = req.body;
+
+    if (!nome && !email && !senha && !nome_loja) {
+        return res.status(404).json('É obrigatório informar ao menos um campo para atualização');
+    }
+
+    try { 
+        let senhaCriptografada;
+
+        if (senha) {
+            senhaCriptografada = await bcrypt.hash(senha, 10);
+        }
+
+        const usuarioAtualizado = await knex('usuarios').update({ nome: nome, email: email, senha: senhaCriptografada, nome_loja: nome_loja }).where({ id: usuario.id }).returning('*');
+
+        if (!usuarioAtualizado) {
+            return res.status(400).json("O usuario não foi atualizado");
+        }
+
+        return res.status(200).json('Usuario foi atualizado com sucesso.');
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
+
+module.exports = {
+    cadastrarUsuario,
+    obterPerfil,
+    atualizarPerfil
+}
